bb0e6e16849a87654ba6197ade9432b02b45f6d3
SamSkinner01 <saskinner19@gmail.com>
2024-01-18 22:41:10 -0500

    removed class, made function instead

--- src/preprocesser.py
+++ src/preprocesser.py
@@ -1,22 +1,18 @@
 import cv2
 
-class Preprocesser:
-    def __init__(self, image):
-        self.image = image
 
-    def preprocess(self):
-        """
-            1. Normalize the image by dividing by 255
-            2. Resize the image to 256x256
-        """
-        self.image = self.image/255
-        self.image = cv2.resize(self.image, (256,256))
-        return self.image
+def preprocess(image):
+    """
+        1. Normalize the image by dividing by 255
+        2. Resize the image to 256x256
+    """
+    image = image/255
+    image = cv2.resize(image, (256,256))
+    return image
     
 if __name__ == '__main__':
     image = cv2.imread('C:\\Users\\sam\\Downloads\\20240118201536_1.jpg')
-    preprocesser = Preprocesser(image)
-    image = preprocesser.preprocess()
+    image = preprocess(image)
     print(image)
     cv2.imshow('image', image)
     cv2.waitKey(0)
